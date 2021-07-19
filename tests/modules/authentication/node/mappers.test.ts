de764ba2a9e8ebfe89684787f0aa5e2ad729c42e
Amo Timothy Kofi <timdereaper1@gmail.com>
2021-07-19 01:49:09 +0000

    test: add test for mappers

---
+++ tests/modules/authentication/node/mappers.test.ts
@@ -0,0 +1,22 @@
+import faker from 'faker';
+import { DBUser } from 'src/base/node/repositories/types';
+import { formatDBUserToAuthUserStructure } from 'src/modules/authentication/node/mappers';
+
+describe('formatDBUserToAuthUserStructure', () => {
+	it('should format db user to authenticated user structure', () => {
+		const dbUser: DBUser = {
+			_id: faker.datatype.uuid(),
+			email: faker.internet.email(),
+			name: faker.name.findName(),
+			password: faker.internet.password(),
+		};
+		const token = faker.random.alphaNumeric();
+		const result = formatDBUserToAuthUserStructure(dbUser, token);
+		expect(result).toEqual({
+			id: dbUser._id,
+			email: dbUser.email,
+			name: dbUser.name,
+			token,
+		});
+	});
+});
